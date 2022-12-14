f63aaa0b3a86fd2860f1d0718e598227b8ca9bd2
JQ <xujacque@gmail.com>
2022-12-14 16:33:24 -0500

    added more styling to about/home section

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,12 @@ import React from 'react'
 export default function About() {
   return (
     <div className="about-container">
-        <div className="intro">
-            <h1> Hello, </h1>
-            <h2> I'm Jacqueline. I go by JQ. </h2>
-            <h3> I'm a self-taught front end developer from NYC. </h3>
+        <img className="flower" src="../img/aflower.png" alt="Rainbow Flower" />
+        <div className="intro w3-animate-opacity">
+            <h1 className="intro1 w3-animate-right"> Hello, </h1>
+            <h2 className="intro2 w3-animate-left"> I'm Jacqueline. 😊 I go by JQ. </h2>
+            <h3 className="intro3 w3-animate-right"> I'm a self-taught front end developer from NYC. </h3>
         </div>
-        <img className="flower" src="../img/flower.png" alt="Rainbow Flower" />
     </div>
   ) 
 }
