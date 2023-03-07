64607598302d98964c2abb7766b3d929320b3f87
JQ <xujacque@gmail.com>
2023-03-07 13:24:12 -0500

    project 1 detail added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,8 +7,13 @@ export default function Projects() {
     <div className="project-container containerPad">
       <div className="project-grid rightMargin">
           <img className="project-images" src="../img/project1.png" alt="JQ's Tea & Coffee Ecommerce"></img>
-          <p className="project-description"> Description</p>
-          <p> </p>
+          <p className="project-description"> 
+            <p className="bold">Project Name: JQ's Coffee & Tea</p>
+            <span> This project is an Ecommerce website built primarily with React & MaterialUI. 
+              I used components, props, and mapping to connect the data. Each page has routing to link to a component. 
+              React state is being used to change the cover image, update the cart items, and store contact information. 
+            </span>
+          </p>
       </div>
       <div className="project-grid rightMargin">
         <img className="project-images" src="../img/project2.jpg" alt="Habit Hamster Keep Track of Your Goals"></img>
