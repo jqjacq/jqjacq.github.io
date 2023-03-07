cf71710d17db26c8be26c2bb6047e738801c50b3
JQ <xujacque@gmail.com>
2023-03-07 14:00:33 -0500

    adjust project spacing, margin, font, and media query

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,11 +6,9 @@ export default function Projects() {
     <header className="centerText w3-animate-left bold"> My Projects </header>
     <div className="project-container containerPad">
       <div className="project-grid rightMargin">
-          <p className="bold">Project Name: JQ's Coffee & Tea
-            <img className="project-images" src="../img/project1.png" alt="JQ's Tea & Coffee Ecommerce"></img>
-          </p>
+        <img className="project-images" src="../img/project1.png" alt="JQ's Tea & Coffee Ecommerce"></img>
           <p className="project-description"> 
-            <p className="bold"> Ecommerce website </p>
+            <h6 className="bold"> Ecommerce website: JQ's Coffee & Tea </h6>
             <span> This project is an Ecommerce website built primarily with React & MaterialUI. 
               I used components, props, and mapping to connect the data. Each page has routing to link to a component. 
               React state is being used to change the cover image, update the cart items, and store contact information. 
@@ -18,11 +16,9 @@ export default function Projects() {
           </p>
       </div>
       <div className="project-grid rightMargin">
-        <p className="bold">Project Name: Habit Hamster
           <img className="project-images" src="../img/project2.jpg" alt="Habit Hamster Keep Track of Your Goals"></img>
-        </p>
         <p className="project-description"> 
-            <p className="bold">Team Project</p>
+            <h6 className="bold">Team Project: Habit Hamster</h6>
             <span> This is a collaborative project designed to help users build better habits. 
               I am working on the front end. 
               My partner is responsible for handling the back end. 
@@ -31,9 +27,11 @@ export default function Projects() {
         </p>
       </div>
       <div className="project-grid rightMargin">
-        <img alt="To Do List"></img>
-        <p> To Do List </p>
-        <p className="project-description"> Description </p>
+        <img className="project-images" alt="To Do List"></img>
+          <p className="project-description"> 
+          <h6 className="bold"> To Do List </h6>
+          <span>Description here.</span>
+        </p>
       </div>
     </div>
     </>
