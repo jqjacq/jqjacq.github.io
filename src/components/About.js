8e0173d9193a262cf4669f987d0746a1dc236d0b
JQ <xujacque@gmail.com>
2023-04-14 14:03:45 -0400

    update additional links and pdf files

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 export default function About() {
   return (
@@ -8,8 +9,10 @@ export default function About() {
         <div className="intro w3-animate-opacity">
             <h1 className=" w3-animate-right"> Hello, 👋</h1>
             <h4 className=" w3-animate-left"> I'm Jacqueline. 😊 I go by JQ. </h4>
-            <h6 className=" w3-animate-right"> I'm a self-taught front end developer from NYC. 🗽 <br></br></h6>
-
+            <h6 className=" w3-animate-right"> I'm a self-taught front end developer from NYC. 🗽</h6>
+            <h6>Check out my <Link to="/projects" className="links"> projects </Link>
+            and my <Link to="/resume" className="links">resume</Link>!
+            </h6>
         <br></br>
         <p className="bold skills-container">Language/Frameworks</p>
           <img className="icons"src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="javascript" width="50" height="50"/>
--- src/components/Media.js
+++ src/components/Media.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Media() {
   return (
     <span className="centerText">
-    <a href="https://www.github.com/jqjacq" alt="Github" >
+    <a href="https://www.github.com/jqjacq" target="popup" alt="Github" >
       <i className="fa-brands fa-2x fa-github socialmedia rightMargin"></i>
     </a>
-    <a href="https://www.linkedin.com" alt="LinkedIn" >
+    <a href="https://www.linkedin.com/in/jqjqjq/" target="popup" alt="LinkedIn" >
       <i className="fa-brands fa-2x fa-linkedin socialmedia rightMargin"></i>
     </a>
     <a href="mailto:xujacque@gmail.com" alt="Email Address" >
